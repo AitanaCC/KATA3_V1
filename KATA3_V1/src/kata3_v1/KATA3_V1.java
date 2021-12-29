061180f2d632f1cb9cbd51d5b0f4549626bcaa35
AitanaCC <96807406+AitanaCC@users.noreply.github.com>
2021-12-29 04:54:42 +0000

    Add files via upload

---
+++ KATA3_V1/src/kata3_v1/KATA3_V1.java
@@ -0,0 +1,12 @@
+package kata3_v1;
+
+public class KATA3_V1 {
+
+    public static void main(String[] args) {
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM"); 
+        histogramDisplay.execute();
+        
+    }
+    
+}
